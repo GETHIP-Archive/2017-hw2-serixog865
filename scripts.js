28586982015e5747c0f9ce7508c08310ebd5210d
serixog865 <serixog865@ops.org>
2016-10-08 09:27:40 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-  $.getJSON("https://raw.githubusercontent.com/GETHIP/HW2-Template/master/grace.json", function(data){
+  $.getJSON("https://raw.githubusercontent.com/GETHIP-Classroom/2017-hw2-serixog865/master/grace.json", function(data){
     console.log(data);
     $("#profile-name").text(data.firstName + " " + data.lastName);
     $("#age").text(data.age);
     $("#profile-picture").attr("src", data.picture);
     $.each(data.topFive, function(key, value){
-      $("#topFive").append("<li>" + value.strength + "</li>");
+		$("#topFive").append("<li>" + value.strength + "</li>");
     });
   });
 });
